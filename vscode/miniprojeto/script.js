0e342c39f56bf904409a40dec471072fa6475aff
macielaraujo <104031118+macielaraujo@users.noreply.github.com>
2022-06-20 20:21:59 -0300

    modificações para  ajuste de ligações

---
+++ vscode/miniprojeto/script.js
@@ -0,0 +1,19 @@
+function entrar(){
+    window.location = 'encaminha.html';
+}
+
+function inicial(){
+    window.location = 'index2.html';
+}
+
+function entrarhtml(){
+    window.location = 'define-html.html';
+}
+
+function entrarcss(){
+    window.location = 'define-css.html';
+}
+
+function entrarjs(){
+    window.location = 'define-js.html';
+}
